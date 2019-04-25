19c1b815ac6d028cccd8e20b07d5900162da9e5c
wyq <393912540@qq.com>
2019-04-25 18:23:59 +0800

    spring boot不启用端口

--- importer/src/main/java/com/dhph/bigdata/importer/ImporterApplication.java
+++ importer/src/main/java/com/dhph/bigdata/importer/ImporterApplication.java
@@ -1,10 +1,10 @@
 package com.dhph.bigdata.importer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.kafka.annotation.EnableKafka;
 
 
@@ -15,7 +15,8 @@ public class ImporterApplication {
 
 
     public static void main(String[] args) {
-		SpringApplication.run(ImporterApplication.class, args);
+//		SpringApplication.run(ImporterApplication.class, args);
+        new SpringApplicationBuilder().sources(ImporterApplication.class).web(false).run(args);
         log.info("Application startup succeed!");
     }
 }
